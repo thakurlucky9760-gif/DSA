8c19cda29b722d3c392f36adf534019653ee7983
thakurlucky9760-gif <thakurlucky9760@gmail.com>
2026-09-04 12:45:03 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (36.98%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low=0;
+        int n=nums.size();
+        int high=n-1;
+
+        while(low<=high){
+            int guess=(low+high)/2;
+
+            if(nums[guess]==target){
+                return guess;
+            }
+
+            if(nums[guess]>nums[n-1]){
+                if(nums[guess]<target){
+                    low=guess+1;
+                }
+                else{
+                    if(nums[0]>target){
+                        low=guess+1;
+                    }
+                    else{
+                        high=guess-1;
+                    }
+                }
+            }
+            else{
+                if(nums[guess]>target){
+                    high=guess-1;
+                }
+                else{
+                    if(nums[n-1]<target){
+                        high=guess-1;
+                    }
+                    else{
+                        low=guess+1;
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+};
